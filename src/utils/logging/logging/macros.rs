b1ee30d8ed63e431af0558108d30168edf4587ab
majiayu000 <1835304752@qq.com>
2026-02-08 15:44:00 +0800

    refactor(utils/logging): remove glob re-exports and use explicit macro path

--- src/utils/logging/logging/macros.rs
+++ src/utils/logging/logging/macros.rs
@@ -10,7 +10,7 @@ macro_rules! log_structured {
                 fields.insert($key.to_string(), serde_json::to_value($value).unwrap_or(serde_json::Value::Null));
             )*
 
-            if let Some(logger) = $crate::utils::logging::async_logger() {
+            if let Some(logger) = $crate::utils::logging::logging::async_logger() {
                 logger.log_structured($level, $logger, $message, fields, None, None);
             }
         }
--- src/utils/logging/mod.rs
+++ src/utils/logging/mod.rs
@@ -6,9 +6,6 @@ pub mod logging;
 pub mod structured;
 pub mod utils;
 
-// Re-export commonly used types and functions
-pub use logging::*;
-pub use structured::*;
 pub use utils::LoggingUtils;
 pub use utils::logger::Logger;
 pub use utils::types::{LogEntry, LogLevel};
