55b7a6463936435c52a785b53c7c416ecda8e002
majiayu000 <1835304752@qq.com>
2026-02-08 15:16:02 +0800

    refactor(traits): remove cache/middleware/secret-manager glob re-exports

--- src/core/traits/mod.rs
+++ src/core/traits/mod.rs
@@ -10,8 +10,5 @@ pub mod provider;
 pub mod secret_manager;
 pub mod transformer;
 
-pub use cache::*;
 pub use error_mapper::*;
-pub use middleware::*;
 pub use provider::*;
-pub use secret_manager::*;
